74e31e2242cedcbd922140e5b39237ad285c58dd
mlhumphriss <91321666+mlhumphriss@users.noreply.github.com>
2023-12-19 03:30:20 +0000

    Update MovementScript.cs
    added annotations

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Simple script used to animate the Jet to fly past volcano
 public class MovementScript : MonoBehaviour
 {
     
-
-    // Update is called once per frame
+    // Update manually moves plane 3 steps a frame to have it fly across screen
     void Update()
     {
         transform.position += new Vector3(3, 0, 0);
